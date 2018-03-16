52eed513885ee2bfc7a9765682d202631da95c85
Kartik Gohil <kartik.gohil@partnerelectronics.com>
2018-03-16 14:20:31 +0000

    fixed bugs in example project and changed number of keys to 2 for testing

--- MIDIKeys.ino
+++ MIDIKeys.ino
@@ -9,20 +9,15 @@
 
 #include "MIDIUSB.h"
 #include "PitchToNote.h"
-#define NUM_BUTTONS  7
+#define NUM_BUTTONS  2
 
-const uint8_t button1 = 2;
-const uint8_t button2 = 3;
-const uint8_t button3 = 4;
-const uint8_t button4 = 5;
-const uint8_t button5 = 6;
-const uint8_t button6 = 7;
-const uint8_t button7 = 8;
+const uint8_t C3 = 2;
+const uint8_t D3b = 3;
 
 const int intensityPot = 0;  //A0 input
 
-const uint8_t buttons[NUM_BUTTONS] = {button1, button2, button3, button4, button5, button6, button7};
-const byte notePitches[NUM_BUTTONS] = {C3, D3, E3, F3, G3, A3, B3};
+const uint8_t buttons[NUM_BUTTONS] = {C3, D3b};
+const byte notePitches[NUM_BUTTONS] = {pitchC3, pitchD3b};
 
 uint8_t notesTime[NUM_BUTTONS];
 uint8_t pressedButtons = 0x00;
